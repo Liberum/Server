02f48e1898f887882b3bbea6a1ebb7c482259bf5
Liberum <al.s@inbox.ru>
2014-03-17 23:06:37 +0200

    liberum
    mounte

--- src/Server.java
+++ src/Server.java
@@ -13,71 +13,80 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 
 public class Server {
-	static String keystore = "C:/Users/al/serverkeys"; // ключ к сертификату
-	// static String keystore = "./src/keys/serverkeys"; // ключ к сертификату
-	static char keystorepass[] = "qwerty".toCharArray();
-	static char keypassword[] = "123456".toCharArray();
+    static String keystore = "C:/serverkeys"; // ключ к сертификату
+    // static String keystore = "./src/keys/serverkeys"; // ключ к сертификату
+    static char keystorepass[] = "qwerty".toCharArray();
+    static char keypassword[] = "123456".toCharArray();
 
-	public static final int HTTPS_PORT = 4443;
+    public static final int HTTPS_PORT = 4443;
 
-	public static void main(String[] args) throws Exception {
-		KeyStore ks = KeyStore.getInstance("JKS");
+    public static void main(String[] args) throws Exception {
+	KeyStore ks = KeyStore.getInstance("JKS");
 
-		ks.load(new FileInputStream(keystore), keystorepass);
-		KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-		kmf.init(ks, keypassword);
-		SSLContext sslcontext = SSLContext.getInstance("SSLv3");
-		sslcontext.init(kmf.getKeyManagers(), null, null);
-		ServerSocketFactory ssf = sslcontext.getServerSocketFactory();
-		final SSLServerSocket serversocket = (SSLServerSocket) ssf
-				.createServerSocket(HTTPS_PORT);
-		new Thread() {
-			public void run() {
-				while (true) {
-					try {
-						Socket s = serversocket.accept();
-						new ProcessingThread(s).start();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}.start();
+	ks.load(new FileInputStream(keystore), keystorepass);
+	KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+	kmf.init(ks, keypassword);
+	SSLContext sslcontext = SSLContext.getInstance("SSLv3");
+	sslcontext.init(kmf.getKeyManagers(), null, null);
+	ServerSocketFactory ssf = sslcontext.getServerSocketFactory();
+	final SSLServerSocket serversocket = (SSLServerSocket) ssf
+		.createServerSocket(HTTPS_PORT);
+	new Thread() {
+	    public void run() {
+		while (true) {
+		    try {
+			Socket s = serversocket.accept();
+			new ProcessingThread(s).start();
+		    } catch (IOException e) {
+			e.printStackTrace();
+		    }
+		}
+	    }
+	}.start();
+    }
+
+    static class ProcessingThread extends Thread {
+
+	private Socket s;
+
+	RunDisk rd = new RunDisk();
+
+	public ProcessingThread(Socket s) {
+	    this.s = s;
 	}
 
-	static class ProcessingThread extends Thread {
+	public void run() {
+	    try {
+		InputStream is = s.getInputStream();
+		OutputStream os = s.getOutputStream();
 
-		private Socket s;
+		ObjectInputStream ois = new ObjectInputStream(is);
 
-		public ProcessingThread(Socket s) {
-			this.s = s;
+		List<String> lst = (List<String>) ois.readObject();
+		System.out.println("Server read {" + lst + "}");
+
+		if (lst.size() == 3) {
+		    rd.mountDskCpt(lst);
+		}
+		if (lst.size() == 1) {
+		    rd.UnMount(lst.get(0));
 		}
 
-		public void run() {
-			try {
-				InputStream is = s.getInputStream();
-				OutputStream os = s.getOutputStream();
-
-				ObjectInputStream ois = new ObjectInputStream(is);
-
-				List<String> lst = (List<String>) ois.readObject();
-				System.out.println("Server read {" + lst + "}");
-				int fin = -1;
-				os.write(fin);
-				os.close();
-				os.flush();
-				// int b;
-				// while ((b = is.read()) != -1) {
-				// b += 10;
-				// os.write(b);
-				// os.flush();
-				// }
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		for (int i = 0; i < lst.size(); i++) {
+		    lst.set(i, "0");
 		}
+
+		int fin = -1;
+		os.write(fin);
+		os.close();
+		os.flush();
+
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    } catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
 	}
+    }
 }
